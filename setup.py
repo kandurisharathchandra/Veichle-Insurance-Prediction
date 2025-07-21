bc17a335bbfc8cb0cb6f10b2b143ff3f41848726
kanduri sharath chandra <sharathchandrakanduri@gmail.com>
2025-07-21 15:16:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Vikash Das",
-    author_email="vikashdas770@gmail.com",
+    author="kanduri sharath chandra",
+    author_email="sharathchandrakanduri@gmail.com",
     packages=find_packages()
 )
